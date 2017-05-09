8304da17d033daf956c784c24977281605e27040
Andy Malkin and Taylor Loftis-Kim <taylor@>
2017-05-09 16:43:17 -0700

    create fully functioning rpg app

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -1,19 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Room } from "../room.model";
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Router } from '@angular/router';
+import { User } from '../user.model';
+import { UserService } from '../user.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
-  styleUrls: ['./room.component.css']
+  styleUrls: ['./room.component.css'],
+  providers: [UserService]
 })
 export class RoomComponent implements OnInit {
+  userId: string;
+  roomId: string;
+  choicesToDisplay;
+  roomToDisplay;
+  rooms: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private router: Router,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe((urlParameters) => {
+      this.userId = urlParameters['id'];
+      this.roomId = urlParameters['roomId'];
+      this.rooms = this.userService.getRooms();
+      this.roomToDisplay = this.userService.getRoomById(this.roomId);
+      this.choicesToDisplay = this.userService.getChoicesByRoomId(this.roomId);
+    })
   }
 
-  goToClickedRoom(clickedRoom: Room) {
-    
+  goToClickedRoom(clickedChoice) {
+    this.router.navigate(['users/' + this.userId + '/rooms/' + clickedChoice[1]]);
   }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -5,13 +5,23 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 @Injectable()
 export class UserService {
   users: FirebaseListObservable<any[]>;
+  rooms: FirebaseListObservable<any[]>;
 
   constructor(private database: AngularFireDatabase) {
     this.users = database.list('users');
+    this.rooms = database.list('rooms');
   }
 
   getUsers() {
     return this.users;
+  }
+
+  getRooms() {
+    return this.rooms;
+  }
+
+  getChoicesByRoomId(roomId: string) {
+    return this.database.object('rooms/' + roomId + '/choices/');
   }
 
   addUser(newUser: User) {
@@ -20,6 +30,10 @@ export class UserService {
 
   getUserById(userId: string) {
     return this.database.object('users/' + userId);
+  }
+
+  getRoomById(roomId: string) {
+    return this.database.object('rooms/' + roomId);
   }
 
 }
